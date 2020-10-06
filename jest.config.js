18dc91c9376a683d0fd2275fc734fb9902410d6f
Ygor Galeno <ygor.tgaleno@gmail.com>
2020-10-06 12:47:18 -0300

    chore: add and configure jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: '@shelf/jest-mongodb',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}

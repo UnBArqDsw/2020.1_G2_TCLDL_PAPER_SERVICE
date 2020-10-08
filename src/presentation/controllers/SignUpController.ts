e87c5d948446792672a3601ac61ddf9bf2181beb
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-08 17:38:25 -0300

    feat: create serverError on HttpHelper

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,6 +1,5 @@
 import { ICreateUser } from '../../domain/iteractor/ICreateUser';
-import { ServerError } from '../errors/ServerError';
-import { successRequest } from '../helpers/HttpHelper';
+import { serverError, successRequest } from '../helpers/HttpHelper';
 import { IController } from '../protocols/IController';
 import { IHttpRequest, IHttpResponse } from '../protocols/IHttp';
 
@@ -16,7 +15,7 @@ export class SignUpController implements IController {
       const user = await this.createUser.execute(request.body);
       return successRequest(user);
     } catch (error) {
-      return { statusCode: 500, body: new ServerError() };
+      return serverError();
     }
   }
 }
--- src/presentation/helpers/HttpHelper.ts
+++ src/presentation/helpers/HttpHelper.ts
@@ -1,6 +1,12 @@
+import { ServerError } from '../errors/ServerError';
 import { IHttpResponse } from '../protocols/IHttp';
 
 export const successRequest = (data: any): IHttpResponse => ({
   statusCode: 200,
   body: data,
+});
+
+export const serverError = (): IHttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(),
 });

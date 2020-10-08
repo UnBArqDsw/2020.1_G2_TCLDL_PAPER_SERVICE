d25915c2fecdded911a691d5ebbd45820462619d
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-08 17:09:36 -0300

    feat: ensure SignUpController returns ServerError if throws

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,4 +1,5 @@
 import { ICreateUser } from '../../domain/iteractor/ICreateUser';
+import { ServerError } from '../errors/ServerError';
 import { successRequest } from '../helpers/HttpHelper';
 import { IController } from '../protocols/IController';
 import { IHttpRequest, IHttpResponse } from '../protocols/IHttp';
@@ -15,7 +16,7 @@ export class SignUpController implements IController {
       const user = await this.createUser.execute(request.body);
       return successRequest(user);
     } catch (error) {
-      return { statusCode: 500, body: new Error('Server error') };
+      return { statusCode: 500, body: new ServerError() };
     }
   }
 }

f1ab3170a3627bfeb7fc2a6f804b735d5cfa8b6d
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-08 17:06:49 -0300

    test: ensure SignUpController returns 500 if throws

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -15,10 +15,10 @@ class CreateUserStub implements ICreateUser {
 describe('SignUpController', () => {
   const createUserStub = new CreateUserStub();
   const sut = new SignUpController(createUserStub);
-  let httpRequest: IHttpRequest;
-  let httpResponse: IHttpResponse;
   describe('when handle execute', () => {
     describe('and promise resolves', () => {
+      let httpRequest: IHttpRequest;
+      let httpResponse: IHttpResponse;
       beforeAll(async () => {
         httpRequest = {
           body: {
@@ -38,6 +38,33 @@ describe('SignUpController', () => {
 
       it('should return user info', () => {
         expect(httpResponse.body).toEqual({ id: 'valid_id', ...httpRequest.body });
+      });
+    });
+
+    describe('and promise rejects', () => {
+      let httpRequest: IHttpRequest;
+      let httpResponse: IHttpResponse;
+
+      beforeAll(async () => {
+        jest.spyOn(createUserStub, 'execute').mockRejectedValueOnce('Error');
+        httpRequest = {
+          body: {
+            name: 'valid_name',
+            lastName: 'valid_lastName',
+            email: 'valid_email',
+            password: 'valid_password',
+            password_confirmation: 'valid_password',
+          },
+        };
+        httpResponse = await sut.handle(httpRequest);
+      });
+
+      it('should return 500 if throws', () => {
+        expect(httpResponse.statusCode).toBe(500);
+      });
+
+      it('should return message error', () => {
+        expect(httpResponse.body).toEqual(new Error('Server error'));
       });
     });
   });

d0143cca3f9524fe557d7e61a4e9d76e5a7324ad
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-09 18:21:09 -0300

    feat: ensure CreateUserAdapter encrypt the password

--- src/data/interactors/CreateUserAdapterDb.ts
+++ src/data/interactors/CreateUserAdapterDb.ts
@@ -1,15 +1,20 @@
 import { User } from '@domain/entities/User';
 import { CreateUser } from '@domain/interactors/CreateUser';
+import { Encrypter } from 'data/protocols/Encrypter';
 import { CreateUserRepository } from 'data/repositories/CreateUserRepository';
 
 export class CreateUserAdapterDb implements CreateUser {
   private readonly createUserRepository: CreateUserRepository
 
-  constructor(createUserRepository: CreateUserRepository) {
+  private readonly encrypter: Encrypter
+
+  constructor(createUserRepository: CreateUserRepository, encrypter: Encrypter) {
     this.createUserRepository = createUserRepository;
+    this.encrypter = encrypter;
   }
 
   async execute(userData: Omit<User, 'id'>): Promise<User> {
-    return this.createUserRepository.execute(userData);
+    const hashedPassword = await this.encrypter.encrypt(userData.password);
+    return this.createUserRepository.execute({ ...userData, password: hashedPassword });
   }
 }

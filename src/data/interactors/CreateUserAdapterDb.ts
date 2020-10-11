cb820bce1565a63ace2fe2a5860778949f2545ad
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-11 17:51:06 -0300

    feat: ensure uuid is generatade on CreateUser interactor

--- src/data/interactors/CreateUserAdapterDb.ts
+++ src/data/interactors/CreateUserAdapterDb.ts
@@ -2,19 +2,26 @@ import { User } from '@domain/entities/User';
 import { CreateUser } from '@domain/interactors/CreateUser';
 import { Encrypter } from '@data/protocols/Encrypter';
 import { CreateUserRepository } from '@data/repositories/CreateUserRepository';
+import { UuidGenerator } from '@data/protocols/UuidGenerator';
 
 export class CreateUserAdapterDb implements CreateUser {
   private readonly createUserRepository: CreateUserRepository
 
   private readonly encrypter: Encrypter
 
-  constructor(createUserRepository: CreateUserRepository, encrypter: Encrypter) {
+  private readonly uuidGenerator: UuidGenerator
+
+  constructor(
+    createUserRepository: CreateUserRepository, encrypter: Encrypter, uuidGenerator: UuidGenerator,
+  ) {
     this.createUserRepository = createUserRepository;
     this.encrypter = encrypter;
+    this.uuidGenerator = uuidGenerator;
   }
 
   async execute(userData: Omit<User, 'id'>): Promise<User> {
     const hashedPassword = await this.encrypter.encrypt(userData.password);
-    return this.createUserRepository.execute({ ...userData, password: hashedPassword });
+    const uuid = await this.uuidGenerator.generate();
+    return this.createUserRepository.execute({ ...userData, id: uuid, password: hashedPassword });
   }
 }

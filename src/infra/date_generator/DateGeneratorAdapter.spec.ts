f53296ca4af4ef91938c8e5d57a4ee8db0f9f6c1
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-12 20:08:16 -0300

    test: ensure DateGeneratorAdapter have correct behavior

---
+++ src/infra/date_generator/DateGeneratorAdapter.spec.ts
@@ -0,0 +1,49 @@
+import datefnstz from 'date-fns-tz';
+import { DateGeneratorAdapter } from './DateGeneratorAdapter';
+
+describe('Date generator adapter', () => {
+  const sut = new DateGeneratorAdapter();
+
+  describe('when calls generate', () => {
+    describe('and promise resolves', () => {
+      let result: string;
+      let mockedDate: Date;
+      const dateFormat = 'yyyy-M-dd HH:mm:ss z';
+      beforeAll(() => {
+        mockedDate = new Date();
+        jest.spyOn(datefnstz, 'utcToZonedTime').mockReturnValueOnce(mockedDate);
+        jest.spyOn(datefnstz, 'format').mockReturnValueOnce('valid_date');
+        result = sut.generate();
+      });
+
+      it('should return a valid date', () => {
+        expect(result).toBe('valid_date');
+      });
+
+      it('format should have been called with correct parameters', () => {
+        expect(datefnstz.format).toHaveBeenCalledWith(mockedDate, dateFormat, {
+          timeZone: 'America/New_York',
+        });
+      });
+      describe('and process.env.TZ is defined', () => {
+        beforeAll(() => {
+          process.env.TZ = 'America/Los_Angeles';
+          mockedDate = new Date();
+          jest.spyOn(datefnstz, 'utcToZonedTime').mockReturnValueOnce(mockedDate);
+          jest.spyOn(datefnstz, 'format').mockReturnValueOnce('valid_date');
+          sut.generate();
+        });
+
+        afterAll(() => {
+          delete process.env.TZ;
+        });
+
+        it('format should have been called with correct parameters', () => {
+          expect(datefnstz.format).toHaveBeenCalledWith(mockedDate, dateFormat, {
+            timeZone: 'America/New_York',
+          });
+        });
+      });
+    });
+  });
+});

cbb4e171457f53a2f24a6c545ae46de289235cd0
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-14 01:39:42 -0300

    feat: create badRequest function on HttpHelper

--- src/presentation/helpers/HttpHelper.ts
+++ src/presentation/helpers/HttpHelper.ts
@@ -1,3 +1,4 @@
+import { BadRequestError } from '@presentation/errors/BadRequestError';
 import { ServerError } from '@presentation/errors/ServerError';
 import { HttpResponse } from '@presentation/protocols/Http';
 
@@ -5,6 +6,15 @@ export const successRequest = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data,
 });
+
+export const badRequest = (message: string): HttpResponse => {
+  const badRequestError = new BadRequestError(message);
+
+  return {
+    statusCode: badRequestError.statusCode,
+    body: badRequestError.message,
+  };
+};
 
 export const serverError = (): HttpResponse => {
   const httpServerError = new ServerError();

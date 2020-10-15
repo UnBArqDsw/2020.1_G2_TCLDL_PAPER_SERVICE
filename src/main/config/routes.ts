bcf5dc8d606068d181e48b0a98b9555cae3d8f9b
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-14 23:42:55 -0300

    refactor: use fast glob to load files name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,13 +1,13 @@
 import { Application, Router } from 'express';
-import { readdirSync } from 'fs';
+import fastGlob from 'fast-glob';
 
 export default (app: Application): void => {
   const router = Router();
   const version = process.env.SERVICE_VERSION || 'v0';
   app.use(`/${version}`, router);
-  readdirSync(`${__dirname}/../routes`).map(async (file) => {
+  fastGlob.sync('**/src/main/routes/**Route.{ts,js}').map(async (file) => {
     if (!file.includes('.test.') && !file.endsWith('.map')) {
-      (await import(`../routes/${file}`)).default(router);
+      (await import(`../../../${file}`)).default(router);
     }
   });
 };

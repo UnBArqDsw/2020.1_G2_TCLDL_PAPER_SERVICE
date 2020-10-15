d1c77c3b30e06fd8f6cec19e176526eed9c0d37e
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-14 23:03:28 -0300

    refactor: correct the error message

--- src/presentation/errors/BadRequestError.ts
+++ src/presentation/errors/BadRequestError.ts
@@ -4,7 +4,7 @@ export class BadRequestError extends Error implements HttpError {
   public readonly statusCode: number
 
   constructor(fields: string) {
-    super(`Invalid request body with error in this fields: ${fields}`);
+    super(`Invalid request body with error in these fields: ${fields}`);
     this.name = this.constructor.name;
     this.statusCode = 400;
   }

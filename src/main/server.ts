cf13f62312aa4d89e69266d45d49f0c1ef0bc28c
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-14 22:03:36 -0300

    feat: setup server

---
+++ src/main/server.ts
@@ -0,0 +1,12 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { typeormConfig } from './ormconfig';
+
+(async () => {
+  await createConnection(typeormConfig);
+
+  const app = (await import('./config/app')).default;
+  const port = process.env.SERVICE_PORT;
+
+  app.listen(port, () => console.log(`Server runing at http://localhost:${port}`));
+})();

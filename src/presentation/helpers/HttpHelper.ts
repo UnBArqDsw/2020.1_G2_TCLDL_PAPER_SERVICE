18fc8bf60e5c5d9b7d2c8ded67280c20e1545f74
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-18 19:59:15 -0300

    feat: add not found http request error

--- src/presentation/helpers/HttpHelper.ts
+++ src/presentation/helpers/HttpHelper.ts
@@ -1,4 +1,5 @@
 import { BadRequestError } from '@presentation/errors/BadRequestError';
+import { NotFoundRequestError } from '@presentation/errors/NotFoundRequestError';
 import { ServerError } from '@presentation/errors/ServerError';
 import { HttpResponse } from '@presentation/protocols/Http';
 
@@ -18,6 +19,15 @@ export const badRequest = (message: string): HttpResponse => {
   return {
     statusCode: badRequestError.statusCode,
     body: badRequestError.message,
+  };
+};
+
+export const notFoundRequest = (message: string): HttpResponse => {
+  const notFoundRequestError = new NotFoundRequestError(message);
+
+  return {
+    statusCode: notFoundRequestError.statusCode,
+    body: notFoundRequestError.message,
   };
 };
 

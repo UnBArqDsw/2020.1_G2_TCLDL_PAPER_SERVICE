f5e7a45427ddfbbe84b72271cdb40fc8b11096ce
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-19 12:15:09 -0300

    feat: use VerifyUserAlreadyExistsMiddleware into SignUpRoute

--- src/main/routes/SignUpRoute.ts
+++ src/main/routes/SignUpRoute.ts
@@ -3,12 +3,18 @@ import { adaptMiddleware } from '@main/adapters/expressMiddlewareAdapter';
 import { SignUpControllerFactory } from '@main/factories/controllers/SignUpControllerFactory';
 import { SignUpValidatorMiddlewareFactory }
   from '@main/factories/middlewares/SignUpValidatorMiddlewareFactory';
+import { VerifyIfUserAlreadyExistsMiddlewareFactory }
+  from '@main/factories/middlewares/VerifyIfUserAlreadyExistsMiddlewareFactory';
 import { Router } from 'express';
 
 const signupController = new SignUpControllerFactory().create();
 const signupValidatorMiddleware = new SignUpValidatorMiddlewareFactory().create();
+const verifyIfUserAlreadyExistsMiddleware = new VerifyIfUserAlreadyExistsMiddlewareFactory()
+  .create();
 
 export default (router: Router) => {
   router.post('/signup',
-    adaptMiddleware(signupValidatorMiddleware), adaptController(signupController));
+    adaptMiddleware(signupValidatorMiddleware),
+    adaptMiddleware(verifyIfUserAlreadyExistsMiddleware),
+    adaptController(signupController));
 };

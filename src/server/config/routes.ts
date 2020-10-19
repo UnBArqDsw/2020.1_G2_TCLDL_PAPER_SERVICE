8cf169ac212ffc03a4216c57e041aa9d1c059811
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-19 19:01:17 -0300

    refactor: put correct folder name

--- src/server/config/routes.ts
+++ src/server/config/routes.ts
@@ -5,7 +5,7 @@ export default (app: Application): void => {
   const router = Router();
   const version = process.env.SERVICE_VERSION || 'v0';
   app.use(`/${version}`, router);
-  fastGlob.sync('**/src/main/routes/**Route.{ts,js}').map(async (file) => {
+  fastGlob.sync('**/src/server/routes/**Route.{ts,js}').map(async (file) => {
     if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../../../${file}`)).default(router);
     }

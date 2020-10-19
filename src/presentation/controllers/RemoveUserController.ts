e07f24358e58349d3fd0ce783866a6daa0fcd5f7
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-19 19:33:15 -0300

    fix: fix implementation

--- src/presentation/controllers/RemoveUserController.ts
+++ src/presentation/controllers/RemoveUserController.ts
@@ -1,19 +1,19 @@
-import { RemoveUserRepository } from '@data/repositories/RemoveUserRepository';
+import { RemoveUser } from '@domain/interactors/RemoveUser';
 import { serverError, successRemove } from '@presentation/helpers/HttpHelper';
 import { Controller } from '@presentation/protocols/Controller';
 import { HttpRequest } from '@presentation/protocols/Http';
 
 export class RemoveUserController implements Controller {
-  private readonly removeUserRepository: RemoveUserRepository
+  private readonly removeUser: RemoveUser
 
-  constructor(removeUserRepository: RemoveUserRepository) {
-    this.removeUserRepository = removeUserRepository;
+  constructor(removeUser: RemoveUser) {
+    this.removeUser = removeUser;
   }
 
   async handle(request: HttpRequest) {
     const { userId } = request.params;
     try {
-      await this.removeUserRepository.execute(userId);
+      await this.removeUser.execute(userId);
       return successRemove();
     } catch (error) {
       return serverError();

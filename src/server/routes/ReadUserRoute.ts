76c7eebf9686943fa7642bcbcbb2c8a36443715f
Gabriel Filipe <gabrielfilipe7unb@gmail.com>
2020-10-20 01:26:17 -0300

    Updating Read User Route

--- src/server/routes/ReadUserRoute.ts
+++ src/server/routes/ReadUserRoute.ts
@@ -1,18 +1,11 @@
 import { ReadUserControllerFactory } from '@server/factories/controllers/ReadUserControllerFactory';
 import { ReadUserValidatorMiddlewareFactory }
   from '@server/factories/middlewares/ReadUserValidatorMiddlewareFactory';
-import { VerifyIfUserAlreadyExistsMiddlewareFactory }
-  from '@server/factories/middlewares/VerifyIfUserAlreadyExistsMiddlewareFactory';
 import { Router } from 'express';
 
 const readUserController = new ReadUserControllerFactory().create();
 const readUserValidatorMiddleware = new ReadUserValidatorMiddlewareFactory().create();
-const verifyIfUserAlreadyExistsMiddleware = new VerifyIfUserAlreadyExistsMiddlewareFactory()
-  .create();
 
 export default (router: Router) => {
-  router.post('/user/:id',
-    readUserValidatorMiddleware,
-    verifyIfUserAlreadyExistsMiddleware,
-    readUserController);
+  router.get('/user/:id', readUserValidatorMiddleware, readUserController);
 };

287c9c50fa3040c24781ecd88110a36eeb2e6918
Guilherme Deusdará <guibanci@gmail.com>
2020-10-19 23:10:32 -0300

    Ajusting express middleware Adapter
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

---
+++ src/main/adapters/expressMiddlewareAdapter.ts
@@ -0,0 +1,20 @@
+import { HttpRequest } from '@presentation/protocols/Http';
+import { Middleware } from '@presentation/protocols/Middleware';
+import { Request, Response, NextFunction } from 'express';
+
+export const adaptMiddleware = (middleware: Middleware) => async (
+  request: Request, response: Response, next: NextFunction,
+) => {
+  const httpRequest: HttpRequest = {
+    body: request.body,
+    params: request.params,
+  };
+
+  const httpResponse = await middleware.handle(httpRequest);
+
+  if (httpResponse.statusCode !== 200) {
+    return response.status(httpResponse.statusCode).json({ error: httpResponse.body });
+  }
+
+  return next();
+};
--- src/server/config/routes.ts
+++ src/server/config/routes.ts
@@ -6,6 +6,7 @@ export default (app: Application): void => {
   const version = process.env.SERVICE_VERSION || 'v0';
   app.use(`/${version}`, router);
   fastGlob.sync('**/src/server/routes/**Route.{ts,js}').map(async (file) => {
+    console.log(file)
     if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../../../${file}`)).default(router);
     }

660dcc4b0e7118ecea02b0279c2b9b45c373d68a
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 01:59:32 -0300

    feat: ensure throws if jwt envs is not seted
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Vitor <sr.meirelesoliveira@gmail.com>
    Co-authored-by: Ygor Galeno <ygor.tgaleno@gmail.com>

--- src/infra/jwt/JwtGeneratorAdapter.ts
+++ src/infra/jwt/JwtGeneratorAdapter.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 export class JwtGeneratorAdapter implements JwtGenerator {
   generate(data: any): string {
-    if (!process.env.JWT_SECRET || process.env.JWT_EXP_TIME) {
+    if (!process.env.JWT_SECRET || !process.env.JWT_EXP_TIME) {
       throw new Error('JWT envs are unseted, set secret an expiration time.');
     }
 

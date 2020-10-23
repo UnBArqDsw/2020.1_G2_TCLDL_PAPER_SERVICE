9f11cda81753aef77e8ae6ae7d689fba19496a41
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 02:14:06 -0300

    feat: ensure password is encrypted
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Vitor <sr.meirelesoliveira@gmail.com>
    Co-authored-by: Ygor Galeno <ygor.tgaleno@gmail.com>

--- src/data/interactors/Authenticate/LoginAdapter.ts
+++ src/data/interactors/Authenticate/LoginAdapter.ts
@@ -2,19 +2,26 @@ import { Login } from '@domain/interactors/Authentication/Login';
 import { Authenticate } from '@domain/entities/Authenticate';
 import { JwtGenerator } from '@data/protocols/JwtGenerator';
 import { FindUserRepository } from '@data/repositories/FindUserRepository';
+import { Encrypter } from '@data/protocols/Encrypter';
 
 export class LoginAdapter implements Login {
   private readonly jwtGenerator: JwtGenerator
 
   private readonly findUserRepository: FindUserRepository
 
-  constructor(jwtGenerator: JwtGenerator, findUserRepository: FindUserRepository) {
+  private readonly encrypter: Encrypter
+
+  constructor(
+    jwtGenerator: JwtGenerator, findUserRepository: FindUserRepository, encrypter: Encrypter,
+  ) {
     this.jwtGenerator = jwtGenerator;
     this.findUserRepository = findUserRepository;
+    this.encrypter = encrypter;
   }
 
   async execute(data: Authenticate): Promise<string> {
-    const user = await this.findUserRepository.execute(data);
+    const password = await this.encrypter.encrypt(data.password);
+    const user = await this.findUserRepository.execute({ ...data, password });
 
     if (!user) {
       throw new Error('User not found.');

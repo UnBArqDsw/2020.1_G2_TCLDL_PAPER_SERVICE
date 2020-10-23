22ae31881c853e04fa6eb11b3105d97c500ddc1d
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 02:15:56 -0300

    refactor: change param type
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Vitor <sr.meirelesoliveira@gmail.com>
    Co-authored-by: Ygor Galeno <ygor.tgaleno@gmail.com>

--- src/infra/db/typeorm/repositories/FindUserRepositoryAdapter.ts
+++ src/infra/db/typeorm/repositories/FindUserRepositoryAdapter.ts
@@ -1,9 +1,10 @@
-import { findUserAttribute, FindUserRepository } from '@data/repositories/FindUserRepository';
+import { FindUserRepository } from '@data/repositories/FindUserRepository';
 import { getRepository } from 'typeorm';
+import { User } from '@domain/entities/User';
 import { UserAdapter } from '../entities/UserAdapter';
 
 export class FindUserRepositoryAdapter implements FindUserRepository {
-  async execute(parameter: findUserAttribute): Promise<UserAdapter | undefined> {
+  async execute(parameter: Partial<Omit<User, 'createdAt' | 'updatedAt'>>): Promise<UserAdapter | undefined> {
     const userRepository = getRepository(UserAdapter);
     return userRepository.findOne({ where: parameter });
   }

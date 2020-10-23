203df77e5ec9b40b0de2491a2485f30e31efb643
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-22 23:11:26 -0300

    feat: ensure LoginAdapter returns a jwt
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- src/data/interactors/Authenticate/LoginAdapter.ts
+++ src/data/interactors/Authenticate/LoginAdapter.ts
@@ -1,8 +1,15 @@
 import { Login } from '@domain/interactors/Authentication/Login';
 import { Authenticate } from '@domain/entities/Authenticate';
+import { JwtGenerator } from '@data/protocols/JwtGenerator';
 
 export class LoginAdapter implements Login {
-  async execute(_data: Authenticate): Promise<string> {
-    return 'valid_token';
+  private readonly jwtGenerator: JwtGenerator
+
+  constructor(jwtGenerator: JwtGenerator) {
+    this.jwtGenerator = jwtGenerator;
+  }
+
+  async execute(data: Authenticate): Promise<string> {
+    return this.jwtGenerator.generate(data);
   }
 }
---
+++ src/data/protocols/JwtGenerator.ts
@@ -0,0 +1,3 @@
+export interface JwtGenerator {
+    generate(data: any): Promise<string>
+}

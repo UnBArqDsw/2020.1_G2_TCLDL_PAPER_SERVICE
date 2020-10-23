bc00639b2fc35838f0ee5453e617d9692ed93aa8
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 01:44:12 -0300

    test: ensure LoginRequestValidator returns false if throws ValidationError
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Vitor <sr.meirelesoliveira@gmail.com>
    Co-authored-by: Ygor Galeno <ygor.tgaleno@gmail.com>

--- src/infra/request_validators/LoginRequestValidator.spec.ts
+++ src/infra/request_validators/LoginRequestValidator.spec.ts
@@ -1,5 +1,6 @@
 import { RequestValidatorReturn } from '@presentation/validators/RequestValidator';
 import { LoginRequestValidator } from './LoginRequestValidator';
+import { ValidationErrorStub } from './stubs/ValidationErrorStub';
 
 const validateAsyncMocked = jest.fn(async () => {});
 jest.mock('joi', () => ({
@@ -22,6 +23,37 @@ describe('Login request validator', () => {
 
       it('should return is valid true', () => {
         expect(result.isValid).toBe(true);
+      });
+    });
+
+    describe('and promise rejects validation error', () => {
+      let result: RequestValidatorReturn;
+      beforeAll(async () => {
+        validateAsyncMocked.mockRejectedValueOnce(new ValidationErrorStub('ValidationError', true, [{
+          message: 'valid_message',
+          path: ['valid_path'],
+          type: 'valid_type',
+          context: {
+            label: 'valid_label',
+          },
+        }]));
+        result = await sut.validate({ test: 'test' });
+      });
+
+      it('should return is valid false', () => {
+        expect(result.isValid).toBe(false);
+      });
+    });
+
+    describe('and promises rejects with an other error', () => {
+      let result: Promise<RequestValidatorReturn>;
+      beforeAll(async () => {
+        validateAsyncMocked.mockRejectedValueOnce(new Error());
+        result = sut.validate({ test: 'test' });
+      });
+
+      it('should return is valid false', () => {
+        expect(result).rejects.toThrow();
       });
     });
   });

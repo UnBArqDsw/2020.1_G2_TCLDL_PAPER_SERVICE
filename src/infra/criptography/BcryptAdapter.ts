8bcc66529ab16098d6c908d59254e22cd31a50a8
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 15:59:01 -0300

    Updates BcryptAdapter
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Vitor <sr.meirelesoliveira@gmail.com>
    Co-authored-by: Ygor Galeno <ygor.tgaleno@gmail.com>

--- src/infra/criptography/BcryptAdapter.ts
+++ src/infra/criptography/BcryptAdapter.ts
@@ -5,4 +5,8 @@ export class BcryptAdapter implements Encrypter {
   async encrypt(value: string): Promise<string> {
     return bcrypt.hash(value, process.env.BCRYPT_SALT || 12);
   }
+
+  async compare(stringEncrypted: string, stringNotEncrypted: string) {
+    return bcrypt.compare(stringEncrypted, stringNotEncrypted);
+  }
 }

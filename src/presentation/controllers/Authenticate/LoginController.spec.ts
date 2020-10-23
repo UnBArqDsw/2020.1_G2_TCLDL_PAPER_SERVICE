8d4d4f952d234eeee7e11a5b6fcd8d9a830c8792
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 00:43:31 -0300

    test: ensure returns 500 if login interactor throws
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Vitor <sr.meirelesoliveira@gmail.com>
    Co-authored-by: Ygor Galeno <ygor.tgaleno@gmail.com>

--- src/presentation/controllers/Authenticate/LoginController.spec.ts
+++ src/presentation/controllers/Authenticate/LoginController.spec.ts
@@ -14,10 +14,9 @@ describe('Login controller', () => {
   const sut: Controller = new LoginController(loginStub);
 
   describe('when call handle', () => {
-    let httpResquest: HttpRequest;
-    let httpResponse: HttpResponse;
-
     describe('and promise resolves', () => {
+      let httpResquest: HttpRequest;
+      let httpResponse: HttpResponse;
       beforeAll(async () => {
         httpResquest = {
           body: {},
@@ -31,6 +30,23 @@ describe('Login controller', () => {
 
       it('should return token in body', () => {
         expect(httpResponse.body).toEqual({ token: 'valid_token' });
+      });
+    });
+
+    describe('and login interactor throws', () => {
+      let httpResquest: HttpRequest;
+      let httpResponse: HttpResponse;
+
+      beforeAll(async () => {
+        jest.spyOn(loginStub, 'execute').mockRejectedValueOnce(new Error());
+        httpResquest = {
+          body: {},
+        };
+        httpResponse = await sut.handle(httpResquest);
+      });
+
+      it('should returns 500', () => {
+        expect(httpResponse.statusCode).toBe(500);
       });
     });
   });

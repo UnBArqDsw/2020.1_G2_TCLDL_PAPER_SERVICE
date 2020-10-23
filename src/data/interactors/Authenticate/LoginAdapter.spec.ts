3bbe205b49071054f5bf896218638ba593e6bd83
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-22 23:45:30 -0300

    test: ensure find user repository and jwt generator are called with correct values
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- src/data/interactors/Authenticate/LoginAdapter.spec.ts
+++ src/data/interactors/Authenticate/LoginAdapter.spec.ts
@@ -1,6 +1,8 @@
 import { Login } from '@domain/interactors/Authentication/Login';
 import { Authenticate } from '@domain/entities/Authenticate';
 import { JwtGenerator } from '@data/protocols/JwtGenerator';
+import { User } from '@domain/entities/User';
+import { FindUserRepository } from '@data/repositories/FindUserRepository';
 import { LoginAdapter } from './LoginAdapter';
 
 class JwtGeneratorStub implements JwtGenerator {
@@ -9,9 +11,24 @@ class JwtGeneratorStub implements JwtGenerator {
   }
 }
 
+class FindUserRepositoryStub implements FindUserRepository {
+  async execute(_data: Partial<Omit<User, 'createdAt' | 'updatedAt'>>): Promise<User | undefined> {
+    return {
+      id: 'valid_id',
+      name: 'valid_name',
+      lastName: 'valid_lastName',
+      email: 'valid_email',
+      password: 'valid_password',
+      createdAt: 'valid_createdAt',
+      updatedAt: 'valid_updatedAt',
+    };
+  }
+}
+
 describe('Login Adapter', () => {
   const jwtGeneratorStub = new JwtGeneratorStub();
-  const sut: Login = new LoginAdapter(jwtGeneratorStub);
+  const findUserRepositoryStub = new FindUserRepositoryStub();
+  const sut: Login = new LoginAdapter(jwtGeneratorStub, findUserRepositoryStub);
   describe('When call execute', () => {
     describe('and promise resolves', () => {
       let result: string;
@@ -21,11 +38,21 @@ describe('Login Adapter', () => {
           email: 'valid_email',
           password: 'valid_password',
         };
+        jest.spyOn(findUserRepositoryStub, 'execute');
+        jest.spyOn(jwtGeneratorStub, 'generate');
         result = await sut.execute(authObject);
       });
 
       it('should return a string token', () => {
         expect(result).toBe('valid_token');
+      });
+
+      it('should call find user repository with correct values', () => {
+        expect(findUserRepositoryStub.execute).toHaveBeenCalledWith(authObject);
+      });
+
+      it('should call jwt generator with correct values', () => {
+        expect(jwtGeneratorStub.generate).toHaveBeenCalledWith({ id: 'valid_id', email: 'valid_email' });
       });
     });
   });

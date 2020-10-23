c7722c257322aa09451ebdec8b86fad68a1b6497
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 00:36:04 -0300

    test: ensure login controller return token in body
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- src/presentation/controllers/Authenticate/LoginController.spec.ts
+++ src/presentation/controllers/Authenticate/LoginController.spec.ts
@@ -1,20 +1,36 @@
 import { Controller } from '@presentation/protocols/Controller';
 import { HttpRequest, HttpResponse } from '@presentation/protocols/Http';
+import { Login } from '@domain/interactors/Authentication/Login';
 import { LoginController } from './LoginController';
 
+class LoginStub implements Login {
+  async execute(_data): Promise<string> {
+    return 'valid_token';
+  }
+}
+
 describe('Login controller', () => {
-  const sut: Controller = new LoginController();
+  const loginStub = new LoginStub();
+  const sut: Controller = new LoginController(loginStub);
+
   describe('when call handle', () => {
     let httpResquest: HttpRequest;
     let httpResponse: HttpResponse;
 
     describe('and promise resolves', () => {
       beforeAll(async () => {
+        httpResquest = {
+          body: {},
+        };
         httpResponse = await sut.handle(httpResquest);
       });
 
       it('should returns 200', () => {
         expect(httpResponse.statusCode).toBe(200);
+      });
+
+      it('should return token in body', () => {
+        expect(httpResponse.body).toEqual({ token: 'valid_token' });
       });
     });
   });

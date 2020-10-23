064f9137d53c2abe95eb9d8e8d27946a3062f81f
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 16:01:55 -0300

    Creates LoginControllerFactory
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Vitor <sr.meirelesoliveira@gmail.com>
    Co-authored-by: Ygor Galeno <ygor.tgaleno@gmail.com>

---
+++ src/server/factories/controllers/LoginControllerFactory.ts
@@ -0,0 +1,12 @@
+import { ControllerFactory } from '@server/protocols/ControllerFactory';
+import { LoginController } from '@presentation/controllers/Authenticate/LoginController';
+import { ExpressControllerAdapter } from '@server/adapters/ExpressControllerAdapter';
+import { LoginFactory } from '../interactors/Authenticate/LoginFactory';
+
+export class LoginControllerFactory implements ControllerFactory {
+  create() {
+    const login = new LoginFactory().create();
+    const loginController = new LoginController(login);
+    return ExpressControllerAdapter.adapt(loginController);
+  }
+}

5711d48efaf2f771296acf60db1f7ba972afa93d
Gabriel <gabrielfilipe7unb@gmail.com>
2020-10-23 00:35:38 -0300

    feat: ensure login controller return token in body
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- src/presentation/controllers/Authenticate/LoginController.ts
+++ src/presentation/controllers/Authenticate/LoginController.ts
@@ -1,8 +1,17 @@
 import { Controller } from '@presentation/protocols/Controller';
 import { HttpResponse, HttpRequest } from '@presentation/protocols/Http';
+import { Login } from '@domain/interactors/Authentication/Login';
+import { successRequest } from '@presentation/helpers/HttpHelper';
 
 export class LoginController implements Controller {
-  async handle(_request: HttpRequest): Promise<HttpResponse> {
-    return { statusCode: 200 };
+  private readonly login: Login
+
+  constructor(login: Login) {
+    this.login = login;
+  }
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    const token = await this.login.execute(request.body);
+    return successRequest({ token });
   }
 }

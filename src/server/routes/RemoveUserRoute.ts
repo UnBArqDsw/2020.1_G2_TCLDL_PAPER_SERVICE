e0ba02c2172b155205ef43638da90c14d79887b5
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-26 20:18:48 -0300

    feat: add auth middleware to remove user route
    Co-authored-by: Gabriel Filipe <gabrielfilipe7unb@gmail.com>
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Vitor <sr.meirelesoliveira@gmail.com>

--- src/server/routes/RemoveUserRoute.ts
+++ src/server/routes/RemoveUserRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { FindUserMiddlewareFactory } from '@server/factories/middlewares/FindUserMiddlewareFactory';
 import { RemoveUserControllerFactory } from '@server/factories/controllers/user/RemoveUserControllerFactory';
+import { AuthMiddlewareFactory } from '@server/factories/middlewares/AuthMiddlewareFactory';
 
+const authMiddleware = new AuthMiddlewareFactory().create();
 const findUserMiddleware = new FindUserMiddlewareFactory().create();
 const removeUserController = new RemoveUserControllerFactory().create();
-
 export default (router: Router) => {
-  router.delete('/users/:userId', findUserMiddleware, removeUserController);
+  router.delete('/users/:userId', authMiddleware, findUserMiddleware, removeUserController);
 };

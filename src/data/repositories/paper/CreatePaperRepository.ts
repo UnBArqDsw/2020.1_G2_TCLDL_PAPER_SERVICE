f1400ce4ffee0e80f194124ab7331db82cf0f5a0
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-11-20 15:42:10 -0300

    feat: create repository protocol to CreatePaperRepository

---
+++ src/data/repositories/paper/CreatePaperRepository.ts
@@ -0,0 +1,4 @@
+import { Repository } from '@data/protocols/Repository';
+import { Paper } from '@domain/entities/Paper';
+
+export interface CreatePaperRepository extends Repository<Paper> {}
--- src/data/repositories/paper/PaperRepository.ts
+++
@@ -1,5 +0,0 @@
-import { Paper } from '@domain/entities/PaperInformation';
-
-export interface PaperRepository {
-  execute: (data: Paper) => Promise<Paper>;
-}

7aab498ae9a918f23b2fb618db067d2369e4932b
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-11-20 01:52:13 -0300

    refactor: add updatedAt to attributes of UpdateUser

--- src/domain/interactors/user/UpdateUser.ts
+++ src/domain/interactors/user/UpdateUser.ts
@@ -2,7 +2,7 @@ import { User } from '@domain/entities/User';
 import { Interactor } from '@domain/protocols/Interactor';
 
 export interface UpdateUserAttribute
-  extends Partial<Pick<User, 'id' | 'name' | 'lastName' |'email' | 'password'>> {}
+  extends Partial<Pick<User, 'id' | 'name' | 'lastName' |'email' | 'password' | 'updatedAt'>> {}
 
 export interface UpdateUser extends
   Interactor<UpdateUserAttribute, User | undefined>{}

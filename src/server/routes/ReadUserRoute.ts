c1cba42a028e87313a039725e36cc985d9d030bf
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-11-20 02:04:06 -0300

    refactor: change path of imports

--- src/server/routes/ReadUserRoute.ts
+++ src/server/routes/ReadUserRoute.ts
@@ -1,11 +1,13 @@
-import { ReadUserControllerFactory } from '@server/factories/controllers/ReadUserControllerFactory';
-import { ReadUserValidatorMiddlewareFactory }
-  from '@server/factories/middlewares/ReadUserValidatorMiddlewareFactory';
+import { ReadUserControllerFactory }
+  from '@server/factories/controllers/user/ReadUserControllerFactory';
+import { AuthMiddlewareFactory } from '@server/factories/middlewares/AuthMiddlewareFactory';
+import { VerifyIfUserIsSameOfDecodedTokenFactory } from '@server/factories/middlewares/VerifyIfUserIsSameOfDecodedTokenFactory';
 import { Router } from 'express';
 
+const authMiddleware = new AuthMiddlewareFactory().create();
+const verifyIfUserIsSameOfDecodedToken = new VerifyIfUserIsSameOfDecodedTokenFactory().create();
 const readUserController = new ReadUserControllerFactory().create();
-const readUserValidatorMiddleware = new ReadUserValidatorMiddlewareFactory().create();
 
 export default (router: Router) => {
-  router.get('/user/:id', readUserValidatorMiddleware, readUserController);
+  router.get('/user/:userId', authMiddleware, verifyIfUserIsSameOfDecodedToken, readUserController);
 };
--- src/server/routes/UpdateUserRoute.ts
+++ src/server/routes/UpdateUserRoute.ts
@@ -1,14 +1,28 @@
-import { UpdateUserControllerFactory } from '@server/factories/controllers/UpdateUserControllerFactory';
-import { UpdateUserValidatorMiddlewareFactory }
-  from '@server/factories/middlewares/UpdateUserValidatorMiddlewareFactory';
-
 import { Router } from 'express';
+import {
+  UpdateUserControllerFactory,
+} from '@server/factories/controllers/user/UpdateUserControllerFactory';
+import {
+  UpdateUserValidatorMiddlewareFactory,
+} from '@server/factories/middlewares/UpdateUserValidatorMiddlewareFactory';
+import { AuthMiddlewareFactory } from '@server/factories/middlewares/AuthMiddlewareFactory';
+import {
+  VerifyIfUserIsSameOfDecodedTokenFactory,
+} from '@server/factories/middlewares/VerifyIfUserIsSameOfDecodedTokenFactory';
+import {
+  FindUserMiddlewareFactory,
+} from '@server/factories/middlewares/FindUserMiddlewareFactory';
 
-const updateUserController = new UpdateUserControllerFactory().create();
+const authMiddleware = new AuthMiddlewareFactory().create();
+const verifyIfUserIsSameOfDecodedToken = new VerifyIfUserIsSameOfDecodedTokenFactory().create();
+const findUserMiddleware = new FindUserMiddlewareFactory().create();
 const updateUserValidatorMiddleware = new UpdateUserValidatorMiddlewareFactory().create();
+const updateUserController = new UpdateUserControllerFactory().create();
 
 export default (router: Router) => {
-  router.put('/updateUser',
-    updateUserValidatorMiddleware,
-    updateUserController);
+  router.put('/user/:userId', authMiddleware, verifyIfUserIsSameOfDecodedToken, findUserMiddleware,
+    updateUserValidatorMiddleware, updateUserController);
+
+  router.patch('/user/:userId', authMiddleware, verifyIfUserIsSameOfDecodedToken, findUserMiddleware,
+    updateUserValidatorMiddleware, updateUserController);
 };

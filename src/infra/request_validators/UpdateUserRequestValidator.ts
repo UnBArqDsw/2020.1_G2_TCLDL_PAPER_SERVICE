ce1ac5470751be20f2587d7500da0e6ba1dd3fb0
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-11-19 23:56:17 -0300

    refactor: ensure UpdateUserValidator have the correct behaviour

--- src/infra/request_validators/UpdateUserRequestValidator.ts
+++ src/infra/request_validators/UpdateUserRequestValidator.ts
@@ -1,12 +1,14 @@
+import { User } from '@domain/entities/User';
 import joi from 'joi';
 import { AbstractRequestValidator } from './AbstractRequestValidator';
 
 export class UpdateUserRequestValidator extends AbstractRequestValidator {
   constructor() {
-    super(joi.object({
-      id: joi.string(),
-      name: joi.string(),
-      lastName: joi.string()
+    super(joi.object<Partial<Pick<User, 'name' | 'lastName' | 'password' | 'email'>>>({
+      email: joi.string().email().optional(),
+      name: joi.string().optional(),
+      lastName: joi.string().optional(),
+      password: joi.string().optional(),
     }));
   }
 }
